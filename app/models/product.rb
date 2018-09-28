0633b4d5ccd6569dc2930f5b7cbf0e6d6e4525e7
Dan Tyekar <dan.tyekar@wolfpack-digital.com>
2018-09-28 13:29:51 +0300

    Creating home-page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  default_scope { order(name: :asc) }
+
   validates :name, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :products
+
+  root to: 'home#index', as: 'home'
 end
