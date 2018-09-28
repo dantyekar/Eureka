16519d3020950a0a91a463203de197c0ddf14c1c
Dan Tyekar <dan.tyekar@wolfpack-digital.com>
2018-09-28 16:36:25 +0300

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   get 'home/index'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+  
   resources :products
+  resources :users
 
   root to: 'home#index', as: 'home'
 end
