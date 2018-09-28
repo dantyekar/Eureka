1562017d1c7322af7f6fca9bd34c3ea1d40ebe11
Dan Tyekar <dan.tyekar@wolfpack-digital.com>
2018-09-28 13:51:18 +0300

    Adding User db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,18 @@ Product.create(name: 'Steaks',
                description: 'Delicious steaks',
                price: 12.50,
                image_url: 'default_food.jpg')
+
+User.create(name: "test1",
+            email: "test1@test.com",
+            password: "parola1234",
+            password_confirmation: "parola1234")
+
+User.create(name: "test2",
+            email: "test2@test.com",
+            password: "parola1234",
+            password_confirmation: "parola1234")
+
+User.create(name: "test",
+            email: "admin@test.com",
+            password: "parola1234",
+            password_confirmation: "parola1234")
